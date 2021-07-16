e433ed1322972f0b9132116e8617a57cfb13ff55
nkkodwani <87091725+nkkodwani@users.noreply.github.com>
2021-07-16 10:37:09 -0500

    Started a card game (war) project to learn classes. This is the deck

---
+++ War_project.py
@@ -0,0 +1,70 @@
+Card_Deck = {
+    'spades_2' : [2, 'spades'],
+    'spades_3' : [3, 'spades'],
+    'spades_4' : [4, 'spades'],
+    'spades_5' : [5, 'spades'],
+    'spades_6' : [6, 'spades'],
+    'spades_7' : [7, 'spades'],
+    'spades_8' : [8, 'spades'],
+    'spades_9' : [9, 'spades'],
+    'spades_10' : [10, 'spades'],
+    'spades_jack' : [11, 'spades'],
+    'spades_queen' : [12, 'spades'],
+    'spades_king' : [13, 'spades'],
+    'spades_ace' : [14, 'spades'],
+    'hearts_2' : [2, 'hearts'],
+    'hearts_3' : [3, 'hearts'],
+    'hearts_4' : [4, 'hearts'],
+    'hearts_5' : [5, 'hearts'],
+    'hearts_5' : [5, 'hearts'],
+    'hearts_6' : [6, 'hearts'],
+    'hearts_7' : [7, 'hearts'],
+    'hearts_8' : [8, 'hearts'],
+    'hearts_9' : [9, 'hearts'],
+    'hearts_10' : [10, 'hearts'],
+    'hearts_jack' : [11, 'hearts'],
+    'hearts_queen' : [12, 'hearts'],
+    'hearts_king' : [13, 'hearts'],
+    'hearts_ace' : [14, 'hearts'],
+    'clubs_2' : [2, 'clubs'],
+    'clubs_3' : [3, 'clubs'],
+    'clubs_4' : [4, 'clubs'],
+    'clubs_5' : [5, 'clubs'],
+    'clubs_6' : [6, 'clubs'],
+    'clubs_7' : [7, 'clubs'],
+    'clubs_8' : [8, 'clubs'],
+    'clubs_9' : [9, 'clubs'],
+    'clubs_10' : [10, 'clubs'],
+    'clubs_jack' : [11, 'clubs'],
+    'clubs_queen' : [12, 'clubs'],
+    'clubs_king' : [13, 'clubs'],
+    'clubs_ace' : [14, 'clubs'],
+    'diamonds_2' : [2, 'diamonds'],
+    'diamonds_3' : [3, 'diamonds'],
+    'diamonds_4' : [4, 'diamonds'],
+    'diamonds_5' : [5, 'diamonds'],
+    'diamonds_6' : [6, 'diamonds'],
+    'diamonds_7' : [7, 'diamonds'],
+    'diamonds_8' : [8, 'diamonds'],
+    'diamonds_9' : [9, 'diamonds'],
+    'diamonds_10' : [10, 'diamonds'],
+    'diamonds_jack' : [11, 'diamonds'],
+    'diamonds_queen' : [12, 'diamonds'],
+    'diamonds_king' : [13, 'diaamonds'],
+    'diamonds_ace' : [14, 'diamonds'],
+}
+
+
+print(Card_Deck)
+
+
+'''if (Card_Deck.get('spades_2') < (Card_Deck.get('spades_3'))):
+    print("navee than")
+else:
+    print("less than")'''
+
+
+
+
+
+
