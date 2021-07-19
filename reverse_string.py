4b2e728d4184534daaa4e0c7f8fbfd86a7ae00b3
nkkodwani <87091725+nkkodwani@users.noreply.github.com>
2021-07-19 16:02:26 -0500

    Delete reverse_string.py

--- reverse_string.py
+++
@@ -1,9 +0,0 @@
-def reverse_string(forward):
-    forward = str(input("Input string to be reversed: "))
-    forward.split()
-    forward = list(forward.split())
-    forward.reverse()
-    print(" ".join(forward))
-
-reverse_string("")
-
