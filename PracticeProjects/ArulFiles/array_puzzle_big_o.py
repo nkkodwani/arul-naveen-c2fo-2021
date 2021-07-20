e55d2cd86511c3df235f958f258d5348ba413a55
arul-sethi-c2fo <arul.sethi@c2fo.com>
2021-07-20 16:21:25 -0500

    Updated the runtime nums list puzzle
    Still has runtime O(n). Now works with test cases where all elements of nums2 > any elements of nums1

--- PracticeProjects/ArulFiles/array_puzzle_big_o.py
+++ PracticeProjects/ArulFiles/array_puzzle_big_o.py
@@ -16,36 +16,65 @@ pseudocode:
 '''
 
 #initialize nums1 and nums2; already sorted
-nums1 = [2, 3, 4, 4, 5, 6, 7, 9, 10, 12, 14, 15, 16, 18, 18, 20, 21, 24, 25, 26, 29]
-nums2 = [0, 1, 4, 5, 5, 7, 9, 11, 12, 14, 14, 15, 16, 18, 19, 21, 24, 28, 32, 32, 33]
+#nums1 = [2, 3, 4, 4, 5, 6, 7, 9, 10, 12, 14, 15, 16, 18, 18, 20, 21, 24, 25, 26, 29]
+#nums2 = [0, 1, 4, 5, 5, 7, 9, 11, 12, 14, 14, 15, 16, 18, 19, 21, 24, 28, 32, 32, 33]
+
+from math import comb
+
+
+nums1 = [1, 2, 3, 3]
+nums2 = [4, 5]
 
 a = 0 #a and b will be used to index through the list and compare
 b = 0
 
-m = len(nums1) - 1
-n = len(nums2) - 1
+m = len(nums1)
+n = len(nums2)
 combined_list = [] #left empty, will append from nums1 and nums2
+median = int(0)
 
 #SORT: compare a with b. add the smaller to the combined list
-while a < m and b < n: 
-    if nums1[a] < nums2[b]:
-        combined_list.append(nums1[a])
-        a+=1
-    elif nums1[a] > nums2[b]:
-        combined_list.append(nums2[b])
-        b+=1
-    elif nums1[a] == nums2[b]:
-        combined_list.append(nums1[a])
-        combined_list.append(nums2[b])
-        a+=1
-        b+=1
-#runtime complexity: O(n)? How do I get this to O(log(n))
+if nums2[0] >= nums1[-1]:
+    combined_list = nums1 + nums2
 
-#now that the combined_list is sorted, the next step is to find the median
+#for nums2[0] < nums1[-1]
+elif nums2[0] < nums1[-1]:
+    while a < m and b < n: 
+        if nums1[a] < nums2[b]: #complexity of 
+            combined_list.append(nums1[a])
+            a+=1
+        elif nums1[a] > nums2[b]:
+            combined_list.append(nums2[b])
+            b+=1
+        elif nums1[a] == nums2[b]:
+            combined_list.append(nums1[a])
+            combined_list.append(nums2[b])
+            a+=1
+            b+=1
+#runtime complexity: O(n); linear with the lengths of lists nums1 and nums2
 
-print(combined_list) 
+print(combined_list)
 
+'''MEDIAN: 
+if (len(combined_list) % 2) == 0:
+    find middle element
+    median = middle element
+    pass
+else: 
+    find 2 middle elements
+    average the 2 middle elements
+    median = middle element 
+'''
 
+if (len(combined_list) % 2 ) != 0:
+    index = int(len(combined_list) / 2) #=2
+    median = combined_list[index]
+
+else: 
+    index = int((len(combined_list) / 2)-1)
+    median = float((combined_list[index] + combined_list[index +1]) / 2)
+
+print(median)
 
 
 
