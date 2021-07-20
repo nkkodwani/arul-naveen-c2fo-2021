f11f2a3c4442e0f8b4a3708448b1d2979615da06
arul-sethi-c2fo <arul.sethi@c2fo.com>
2021-07-20 16:24:12 -0500

    Updated runtime lists puzzle one more time
    Cleaned up comments and code

--- PracticeProjects/ArulFiles/array_puzzle_big_o.py
+++ PracticeProjects/ArulFiles/array_puzzle_big_o.py
@@ -6,24 +6,11 @@
 a function w/ log time: 
 - the choice of the next elemenet on which to perform some action is one of several possibilities
 - only one will need to be chosen
-
-pseudocode:
-- get lengths of each list
-- combine the sorted lists
-- get length of new list
-- sort by dividing length by 2 and picking the median value; create a case for even/odd lengths
-- print median
 '''
 
 #initialize nums1 and nums2; already sorted
-#nums1 = [2, 3, 4, 4, 5, 6, 7, 9, 10, 12, 14, 15, 16, 18, 18, 20, 21, 24, 25, 26, 29]
-#nums2 = [0, 1, 4, 5, 5, 7, 9, 11, 12, 14, 14, 15, 16, 18, 19, 21, 24, 28, 32, 32, 33]
-
-from math import comb
-
-
-nums1 = [1, 2, 3, 3]
-nums2 = [4, 5]
+nums1 = [2, 3, 4, 4, 5, 6, 7, 9, 10, 12, 14, 15, 16, 18, 18, 20, 21, 24, 25, 26, 29]
+nums2 = [0, 1, 4, 5, 5, 7, 9, 11, 12, 14, 14, 15, 16, 18, 19, 21, 24, 28, 32, 32, 33]
 
 a = 0 #a and b will be used to index through the list and compare
 b = 0
@@ -33,14 +20,15 @@ n = len(nums2)
 combined_list = [] #left empty, will append from nums1 and nums2
 median = int(0)
 
-#SORT: compare a with b. add the smaller to the combined list
+#SORT: 
+# If all elements in nums2 > any elements in nums1, then combine lists. 
+# Otherwise, compare a with b. add the smaller to the combined list
 if nums2[0] >= nums1[-1]:
     combined_list = nums1 + nums2
 
-#for nums2[0] < nums1[-1]
 elif nums2[0] < nums1[-1]:
     while a < m and b < n: 
-        if nums1[a] < nums2[b]: #complexity of 
+        if nums1[a] < nums2[b]: 
             combined_list.append(nums1[a])
             a+=1
         elif nums1[a] > nums2[b]:
