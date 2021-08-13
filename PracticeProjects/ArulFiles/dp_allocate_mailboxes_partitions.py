876e0c464f5530fd4c57cb2b117d2be8710bfa63
Arul <59489321+arulsethi02@users.noreply.github.com>
2021-08-13 11:42:53 -0500

    Finished allocate mailbox code using partitioning
    Learned partitioning method from discussion forum on LC

--- PracticeProjects/ArulFiles/dp_allocate_mailboxes_partitions.py
+++ PracticeProjects/ArulFiles/dp_allocate_mailboxes_partitions.py
@@ -15,15 +15,13 @@ OUTPUT:
         mailboxes placed at 3 and 20; minimum output = 4 = (3-1) + (5-3) + (20-20) 
 
 PSEUDOCODE:
-    k = 1 : put mailbox in median (given by leetcode hint)
-    partition!
-    break houses into k groups and store results
-        from leetcode discussion: form one group, then form k-1 groups with remaining elements
-    to do this: vvvvvv
+    Base Cases: 
+        k = 1 : put mailbox in median (given by leetcode hint)
+        k = # of houses : put one mailbox at each house (total distance is 0)
 
-    need to form k total groups: 
-    create one group: length is maximum of (len(houses) - (k-1)) 
-    from k-1 groups out of remaining houses not in group1
+    partition: 
+    break houses into k groups --> this makes it so each group has one mailbox (base case)
+        from leetcode discussion: form one group, then form k-1 groups with remaining elements (recursion)
 
 
 
@@ -45,39 +43,48 @@ k = int(input("Input the number of mailboxes: "))
 #CREATE MEMO TABLE: 
 a1 = np.zeros(shape=(houses_pos[-1], houses_pos[-1]))
 a1.fill(-1)
-#right now this is kinda useless
+#right now this is not being used
 
 
 #start index will be first house pos
 #end index will be last house pos
-def allocate_mailboxes(start: int, end : int, k : int): #start and end are indexes in the houses_pos list
-
-    # total_dist = 1000000
+#k will be mailboxes in the group
+def allocate_mailboxes(start: int, end : int, k : int): 
 
     #Base case: if there is one mailbox, place it at the median house (hint from LC) 
     if k == 1: 
         mid_index = int(st.median([start, end]))
         total_dist = 0
         for i in range(start, end + 1): 
-            total_dist += (abs(houses_pos[i] - houses_pos[mid_index]))
+            total_dist += (abs(houses_pos[i] - houses_pos[mid_index])) #adds total distance from each house to mailbox
         return total_dist
     
-    #if there is a mailbox for every house in the group, then 
+    #Another base case: if there is a mailbox for every house in the group, then total distance will be 0
     if k == (end - start) + 1:
         return 0
 
-    #need to break houses into k subgroups
-    #first create a group1
+   
 
     total_dist = 10000000
 
-    for i in range (start, end): #+2 because we want (len(houses) - (k-1)) to be included in the range
-        total_dist = min(total_dist, allocate_mailboxes(start, i, 1) + allocate_mailboxes(i, end, k-1))
+    #need to break houses into k subgroups. Each subgroup gets one mailbox
+    for i in range (start, end): #tries every possible partition of the houses  
+        inst_dist = allocate_mailboxes(start, i, 1) + allocate_mailboxes(i+1, end, k-1)
+        #replace total distance if instant distance is 
+        if inst_dist < total_dist: 
+            total_dist = inst_dist
 
-        '''group1 = houses_pos[0:i]
-        group2 = houses_pos[i:]
-        print(group1, group2)
-        #form k-1 groups out of remaining houses not in group1'''
+        '''
+        each time the loop runs, the program partitions the houses list. the idea is to make two groups: the first group is given one mailbox (first 'allocate_mailboxes') and the second group is given
+            the rest of the mailboxes. Recursion is used to continue partitioning the second group over and over until the mailboxes for that group = 1 (therefore recurses k-1 times). When the mailboxes
+            for the second group == 1, the method finds the median house in the group and places the mailbox there.  
+        '''
+
+        #old code to show the partition
+        #   group1 = houses_pos[start:i]
+        #   group2 = houses_pos[i:end]
+        #   print(group1, group2)
+        #form k-1 groups out of group2
 
     return total_dist
 
